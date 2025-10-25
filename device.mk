6221d1e30fc6b304551aa9bdb94d1f60022484c0
Mashopy <eliasgheeraert@gmail.com>
2025-10-25 19:24:58 +0200

    X3: Enable MediaTek stability changes for wpa_supplicant

--- device.mk
+++ device.mk
@@ -301,6 +301,8 @@ PRODUCT_PACKAGES += \
     vndservicemanager
 
 # WiFi
+$(call soong_config_set,wpa_supplicant_8,board_wlan_mediatek_stability,true)
+
 PRODUCT_PACKAGES += \
     wpa_supplicant \
     hostapd \
