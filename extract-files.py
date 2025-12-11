408ca989122a47a5d7e5a906c25abf2a23246cf6
Michael Bestas <mkbestas@lineageos.org>
2025-12-11 13:30:36 +0200

    X3: Patch some blobs to depend on libtinyxml2-v34.so
    libtinyxml2 10.1.0 update which landed in BP4A contains ABI
    incompatible changes which cause runtime crashes.
    
    Change-Id: Icc67da5f754b0bb30dce6192b1e520fdf1349ac5

--- extract-files.py
+++ extract-files.py
@@ -72,8 +72,17 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/libnvram.so'
     ): blob_fixup()
         .add_needed('libbase_shim.so'),
+    (
+        'vendor/lib64/hw/mt6835/vendor.mediatek.hardware.pq_aidl-impl.so',
+        'vendor/lib64/lib_power_applist.so',
+        'vendor/lib64/librt_extamp_intf.so',
+        'vendor/lib64/libpowerhal.so',
+        'vendor/lib64/libpqxmlparser.so'
+    ): blob_fixup()
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
     'vendor/lib64/hw/audio.primary.mediatek.so': blob_fixup()
         .replace_needed('libalsautils.so', 'libalsautils-v33.so')
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so')
         .binary_regex_replace(b'A2dpsuspendonly', b'A2dpSuspended\x00\x00')
         .binary_regex_replace(b'BTAudiosuspend', b'A2dpSuspended\x00'),
     'vendor/lib64/hw/hwcomposer.mtk_common.so' : blob_fixup()
