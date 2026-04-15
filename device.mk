a14b483fd70222891da0a81dd2e94ebca32082b2
Mashopy <eliasgheeraert@gmail.com>
2026-04-15 23:26:34 +0200

    X3: Inherit common MediaTek overlays

--- device.mk
+++ device.mk
@@ -215,11 +215,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
 # Overlays
+$(call inherit-product, hardware/mediatek/overlay/mssi.mk)
+
 PRODUCT_PACKAGES += \
     FrameworksResOverlay \
-    SettingsResOverlay \
-    TelephonyResOverlay \
-    WifiResOverlay
+    SettingsResOverlay
 
 PRODUCT_PACKAGES += \
     NcmTetheringOverlay
